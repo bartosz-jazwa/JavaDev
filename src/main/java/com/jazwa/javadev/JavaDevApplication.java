bdc5eb5a54d35bb7ed170a9c1b281f06e0169280
bartek <jazzbar@onet.eu>
2019-05-01 18:22:28 +0200

    Initial commit

---
+++ src/main/java/com/jazwa/javadev/JavaDevApplication.java
@@ -0,0 +1,13 @@
+package com.jazwa.javadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevApplication.class, args);
+    }
+
+}
